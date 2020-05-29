708ddd62d0ee4ff87174ec953840a3fa6178b6e2
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2020-05-29 23:35:22 +0900

    vueinstall

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">ようこそ！</div>
+         <!--ループ処理-->
+     <ul class="list-group">
+        <li class="list-group-item" v-for="(todo, index) in list">
+            @{{ todo.task }}
+            <span class="pull-right">
+                <button class="btn btn-xs btn-info" v-on:click="edit(index);">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                </button>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    ログインしました！
-                </div>
-            </div>
-        </div>
+                <button class="btn btn-xs btn-warning" v-on:click="remove(todo.created_at,index);"> <!--todo.id は undefined になる-->
+                    <span class="glyphicon glyphicon-trash"></span>
+                </button>
+            </span>
+        </li>
+     </ul>
     </div>
 </div>
 @endsection
